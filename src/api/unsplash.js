97d97d56e7ff895f4debb7735ef3833a88fd0374
Julie Brown <juliebrown26@gmail.com>
2020-02-18 17:54:03 -0700

    updated gitignore

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID Powpi1uKQ5T975Ur_2mbLw_u6lQqBocqeV1rg_B4gCA"
-  }
-});
